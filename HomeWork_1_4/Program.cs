7c8bfef773e38a164c871d29f5e75a5e5a645215
tatii1111 <tanuta1111@mail.ru>
2025-03-13 20:42:03 +0300

    Решение 4

---
+++ HomeWork_1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace task_1_4_1;
+
+class Program
+{
+    static void Main()
+    {
+     Console.WriteLine("Введите натуральное число");
+     Console.Write("n = ");
+     int n = Math.Abs(int.Parse(Console.ReadLine()));
+     int k = n;
+     for (int i = 1; i < n; i++)
+         k = k * i;
+     Console.WriteLine(k);
+    }
+}
