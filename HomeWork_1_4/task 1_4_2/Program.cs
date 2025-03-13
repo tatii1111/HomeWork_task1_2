e217a98f81d06dfa8138fef6243f77c488ac3e4e
tatii1111 <tanuta1111@mail.ru>
2025-03-13 20:30:37 +0300

    Задачи тема 4

---
+++ HomeWork_1_4/task 1_4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task_1_4_2;
+
+class Program
+{                                                       
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное число");
+        Console.Write("n = ");
+        int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+        double k = Math.Pow(n, -1);
+        
+        for ( int i = 1; i < n ; i++)
+            k = k + Math.Pow(i, -1);
+            
+        Console.WriteLine(k);
+    }
+}
---
+++ HomeWork_1_4/task 1_4_6/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace task_1_4_6;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+        Console.Write("n = ");
+        double n = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Stepen(n)
+            ? "Введенное число n является степенью 2"
+            : "Введенное число n не является степенью 2");
+    }
+
+    static bool Stepen(double n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        else if (n > 1)
+        {
+            while (n > 1)
+            {
+                if (n % 2 != 0)
+                {
+                    return false;
+                }
+
+                n /= 2;
+            }
+
+            return true;
+        }
+        else
+        {
+            while (n < 1)
+            {
+                n *= 2;
+            }
+
+            return n == 1;
+        }
+    }
+}
+    
+    
+        
+            
